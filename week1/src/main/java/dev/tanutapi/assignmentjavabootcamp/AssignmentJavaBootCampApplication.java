993882e95e05e43629e6741dd6cf7d131c1475c9
Tanut Apiwong <tanutapi@gmail.com>
2022-02-16 21:00:25 +0700

    Init Spring Boot project #1

---
+++ week1/src/main/java/dev/tanutapi/assignmentjavabootcamp/AssignmentJavaBootCampApplication.java
@@ -0,0 +1,13 @@
+package dev.tanutapi.assignmentjavabootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentJavaBootCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentJavaBootCampApplication.class, args);
+	}
+
+}
