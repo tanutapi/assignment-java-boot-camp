758c8e0a3d41163eef95ccfa79b8325feff60c2d
Tanut Apiwong <tanutapi@gmail.com>
2022-02-19 20:24:15 +0700

    feat: mock the user's shipping address

--- week1/src/main/java/dev/tanutapi/assignmentjavabootcamp/AssignmentJavaBootCampApplication.java
+++ week1/src/main/java/dev/tanutapi/assignmentjavabootcamp/AssignmentJavaBootCampApplication.java
@@ -1,6 +1,10 @@
 package dev.tanutapi.assignmentjavabootcamp;
 
+import dev.tanutapi.assignmentjavabootcamp.user.User;
+import dev.tanutapi.assignmentjavabootcamp.user.UserRepository;
 import dev.tanutapi.assignmentjavabootcamp.user.UserService;
+import dev.tanutapi.assignmentjavabootcamp.userShippingAddress.UserShippingAddress;
+import dev.tanutapi.assignmentjavabootcamp.userShippingAddress.UserShippingAddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,11 +21,25 @@ public class AssignmentJavaBootCampApplication {
 	@Autowired
 	private UserService userService;
 
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private UserShippingAddressRepository userShippingAddressRepository;
+
 	@PostConstruct
 	void prepareData() {
 		try {
-			userService.createUser("tanutapi", "qwerty", "Tanut", "Apiwong");
-			userService.createUser("hugo", "abc123", "Hugo", "Man");
+			User user1 = userService.createUser("tanutapi", "qwerty", "Tanut", "Apiwong");
+			User user2 = userService.createUser("hugo", "abc123", "Hugo", "Man");
+			UserShippingAddress userShippingAddress1 = new UserShippingAddress(user1, "Tanut Apiwong", "123 street road", "Chatujak", "Bangkok", "10900", "0812345678");
+			UserShippingAddress userShippingAddress2 = new UserShippingAddress(user2, "Hugo Man", "789 1st-street road", "Sathorn", "Bangkok", "10120", "0898765432");
+			userShippingAddressRepository.save(userShippingAddress1);
+			userShippingAddressRepository.save(userShippingAddress2);
+			user1.setUserShippingAddress(userShippingAddress1);
+			user2.setUserShippingAddress(userShippingAddress2);
+			userRepository.save(user1);
+			userRepository.save(user2);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
