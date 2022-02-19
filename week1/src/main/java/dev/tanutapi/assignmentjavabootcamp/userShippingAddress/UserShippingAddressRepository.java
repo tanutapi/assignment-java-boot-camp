294eb90336b8eaf04c6a2bcf3206bacab43c2b20
Tanut Apiwong <tanutapi@gmail.com>
2022-02-19 20:24:33 +0700

    feat: mock the user's shipping address

---
+++ week1/src/main/java/dev/tanutapi/assignmentjavabootcamp/userShippingAddress/UserShippingAddressRepository.java
@@ -0,0 +1,6 @@
+package dev.tanutapi.assignmentjavabootcamp.userShippingAddress;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserShippingAddressRepository extends JpaRepository<UserShippingAddress, Integer> {
+}
