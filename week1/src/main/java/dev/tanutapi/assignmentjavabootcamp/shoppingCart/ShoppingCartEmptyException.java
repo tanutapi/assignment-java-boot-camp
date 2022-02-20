e62c63755422147eaf13c99727dd79bde41cc569
Tanut Apiwong <tanutapi@gmail.com>
2022-02-20 17:48:54 +0700

    fix: update assert

--- week1/src/main/java/dev/tanutapi/assignmentjavabootcamp/shoppingCart/ShoppingCartEmptyException.java
+++ week1/src/main/java/dev/tanutapi/assignmentjavabootcamp/shoppingCart/ShoppingCartEmptyException.java
@@ -3,7 +3,7 @@ package dev.tanutapi.assignmentjavabootcamp.shoppingCart;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Shopping cart is empty")
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Shopping cart is empty")
 public class ShoppingCartEmptyException extends RuntimeException {
     public ShoppingCartEmptyException(String s) {
         super(s);
